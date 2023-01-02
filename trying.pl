392f83a71ed7198ffa81f87611dd6929a90f9f70
miguelcfsilva11 <miguelcfsilva@gmail.com>
2023-01-02 20:59:48 +0000

    pushing min max start

--- trying.pl
+++ trying.pl
@@ -63,46 +63,46 @@ expandChildren([Child|Rest],Depth):-
     expandChildren(Rest,Depth).
 
   
-evaluate_board([Board, player2|Rest], Score):-
-    getPlayerPieces([Board,player2|Rest], [P2,_]),
-    getPlayerPieces([Board,player1|Rest], [_,_]),
-    Score = P2.
+evaluate_board([Board, _|Rest], Score):-
+    getPlayerPieces([Board, player2|Rest], [_,CapturedPieces2]),
+    getPlayerPieces([Board, player1|Rest], [_,CapturedPieces1]),
+    Score = (CapturedPieces2 - CapturedPieces1) * 100.
 
-evaluate_board([Board, player1|Rest], Score):-
-    getPlayerPieces([Board,player2|Rest], [_,N]),
-    getPlayerPieces([Board,player1|Rest], [_,M]),
-    Score = 10 * M - (10 * N).
-
-% node(State,Value)
-% link(State,[State,Move])
-numberB(1, 1).
-
-printNumber(X, Y):-
-    numberB(1, Y),
-    X = X + 1.  
 
 
 min_max(State,0):-
     evaluate_board(State,BestValue),
+    getBoard(State,Board),
+    printBoard(Board),nl,
     retract(node(State,0)),
     asserta(node(State,BestValue)).
 
 min_max(State,Depth):-
     findall(ChildState,(node(ChildState,Value),link(ChildState,State,Move)),Children),
     Children == [],!.
-min_max(State,Depth):-
+
+min_max(State,Depth, BestMove):-
+    getPlayer(State, player1),!,
     getBoard(State,Board),
     printBoard(Board),nl,
     findall(ChildState,(node(ChildState,Value),link(ChildState,State,Move)),Children),!,
     Depth1 is Depth - 1,
-    min_max_list(Children,Depth1),!,
+    min_max_list(Children,Depth1, BestMove),!,
     findall(Value-Move,(node(ChildState,Value),link(ChildState,State,Move)),ValueMoves),!,
-    write(ValueMoves),nl.
+    at(0, ValueMoves, BestMove).
     
 
-
+min_max(State,Depth, BestMove):-
+    getBoard(State,Board),
+    printBoard(Board),nl,
+    findall(ChildState,(node(ChildState,Value),link(ChildState,State,Move)),Children),!,
+    Depth1 is Depth - 1,
+    min_max_list(Children,Depth1, BestMove),!,
+    findall(Value-Move,(node(ChildState,Value),link(ChildState,State,Move)),ValueMoves),!,
+    length(ValueMoves, L),
+    at(L, ValueMoves, BestMove).
 
 min_max_list([],_):-!.
-min_max_list([Child|Children],Depth):-
-    min_max(Child,Depth),!,
+min_max_list([Child|Children],Depth, BestMove):-
+    min_max(Child,Depth, BestMove),!,
     min_max_list(Children,Depth).
